014191718eeb1ebb28dc7a03375ef19729b8d950
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2017-03-12 12:26:03 +0100

    do not purge compiler tools

--- script/minimize.sh
+++ script/minimize.sh
@@ -11,28 +11,28 @@ echo "==> Installed packages before cleanup"
 dpkg --get-selections | grep -v deinstall
 
 # Remove some packages to get a minimal install
-echo "==> Removing all linux kernels except the currrent one"
-dpkg --list | awk '{ print $2 }' | grep -e 'linux-\(headers\|image\)-.*[0-9]\($\|-generic\)' | grep -v "$(uname -r | sed 's/-generic//')" | xargs apt-get -y purge
-echo "==> Removing linux source"
-dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
-echo "==> Removing development packages"
-dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
-echo "==> Removing documentation"
-dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
-echo "==> Removing development tools"
+#echo "==> Removing all linux kernels except the currrent one"
+#dpkg --list | awk '{ print $2 }' | grep -e 'linux-\(headers\|image\)-.*[0-9]\($\|-generic\)' | grep -v "$(uname -r | sed 's/-generic//')" | xargs apt-get -y purge
+#echo "==> Removing linux source"
+#dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+#echo "==> Removing development packages"
+#dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+#echo "==> Removing documentation"
+#dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
+#echo "==> Removing development tools"
 #dpkg --list | grep -i compiler | awk '{ print $2 }' | xargs apt-get -y purge
 #apt-get -y purge cpp gcc g++ 
-apt-get -y purge build-essential git
-echo "==> Removing default system Ruby"
-apt-get -y purge ruby ri doc
-echo "==> Removing default system Python"
-apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
-echo "==> Removing X11 libraries"
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
-echo "==> Removing obsolete networking components"
-apt-get -y purge ppp pppconfig pppoeconf
-echo "==> Removing other oddities"
-apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
+#apt-get -y purge build-essential git
+#echo "==> Removing default system Ruby"
+#apt-get -y purge ruby ri doc
+#echo "==> Removing default system Python"
+#apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
+#echo "==> Removing X11 libraries"
+#apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+#echo "==> Removing obsolete networking components"
+#apt-get -y purge ppp pppconfig pppoeconf
+#echo "==> Removing other oddities"
+#apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
 
 # Clean up the apt cache
 apt-get -y autoremove --purge
